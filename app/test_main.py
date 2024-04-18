be1c9f82efb84bf86b508213183dbe801a062369
Marko Panchak <marko.panchak1@gmail.com>
2024-04-18 21:52:35 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,16 @@
+import pytest
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "words, result",
+    [
+        pytest.param("", True, id="empty string is an isogram"),
+        pytest.param("playgrounds", True, id="All letters is different"),
+        pytest.param("look", False, id="This word has same letters"),
+        pytest.param("Adam", False, id="This word has same letters"),
+
+    ]
+)
+def test_isogram(words: str, result: bool) -> None:
+    assert is_isogram(words) == result
